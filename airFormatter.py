9f2f5799911ea389e0c11922c35d9847165b37dc
kyle.ceguera <kyle.ceguera@ttc.com>
2025-03-11 15:43:13 -0700

    updating clear button and chaning its color

--- airFormatter.py
+++ airFormatter.py
@@ -199,14 +199,40 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+st.markdown("""
+    <style>
+        /* Target the button with a specific key ('clear_button') */
+        .stButton[data-baseweb="button"][key="clear_button"]>button {
+            background-color: red;
+            color: white;
+            border: none;
+        }
+		.stButton[data-baseweb="button"][key="clear_button"]>button:hover {
+        background-color: red; 
+        color: white;
+		border: 2px solid white;
+        border-radius: 10px;
+        height: 3em;
+        width: 12em;
+		}
+		.stButton[data-baseweb="button"][key="clear_button"]>button:focus {
+        outline: none; /* Remove the outline */
+		background-color: red; 
+        border: none; /* Remove any border */
+		color: white;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 with col1:
 	sked_source = st.selectbox(label='Schedule Source - Where did you get your schedule from?',options=['Tropics', 'Air Department'], placeholder='Tropics', help="Selection dictates how the schedule gets parsed")
 	st.write("Paste your schedule below and the software will format it for you.")
-	data = st.text_area(label='inputted schedule',label_visibility='collapsed', height=250)
+	data = st.text_area(label='inputted schedule',label_visibility='collapsed', height=250, value=st.session_state.text)
 	st.session_state.text = data
 	format = st.button("Format Flight Schedule")
-	if st.button('Clear'):
+	if st.button('Clear', key='clear_button'):
 		st.session_state.text = ''
+		
 
 def format_airdept_flights(text):
 	air_dept_pattern = r'^\s*([A-Za-z]{2}\d{1,5})\s+([A-Za-z])\s+([A-Za-z]+)\s+([A-Za-z]{3})\s+([A-Za-z]{3})\s+([0-9]{2}[A-Za-z]{3})\s+([0-9]{4})\s+([0-9]{2}[A-Za-z]{3})\s+([0-9]{4})\s+([A-Za-z]{2})\s+0\s+([A-Za-z\s]+)\s*$'
